13bac3b849d70517771c1ce165290b25a3e2e9a9
Deepak <deepak.negi343@gmail.com>
2020-08-27 17:02:31 +0530

    Updates

---
+++ precache-manifest.909ec3f051c068a33cb5876294337b4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5066c86df085ba85313914714a81c6b2",
+    "url": "/movie_app_react/index.html"
+  },
+  {
+    "revision": "c12ca715d1b9d0336d3a",
+    "url": "/movie_app_react/static/css/main.fc85b604.chunk.css"
+  },
+  {
+    "revision": "e412fc8ee693c7c9617f",
+    "url": "/movie_app_react/static/js/2.2217147c.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/movie_app_react/static/js/2.2217147c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c12ca715d1b9d0336d3a",
+    "url": "/movie_app_react/static/js/main.9621b860.chunk.js"
+  },
+  {
+    "revision": "e6d9f6f44ba9bedfca14",
+    "url": "/movie_app_react/static/js/runtime-main.eb728cfc.js"
+  }
+]);
